f23ac75f19fd1000ae12a3877ee1c4a0e818605e
youngman <sksn12@naver.com>
2022-06-12 19:54:52 +0900

    mod : env삭제

--- src/pages/Serach/Input/index.tsx
+++ src/pages/Serach/Input/index.tsx
@@ -1,14 +1,33 @@
 import styles from './input.module.scss'
 import { BiSearchAlt2 } from 'react-icons/bi'
+import { useRecoilState } from 'recoil'
+import { SearchValueState } from 'states/search'
+import { ChangeEvent, FormEvent } from 'react'
 
 const Input = () => {
+  const [content, setContent] = useRecoilState(SearchValueState)
+
+  const handleContentChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget
+    setContent(value)
+  }
+
+  const handleSubmit = (e?: FormEvent<HTMLFormElement>) => {
+    e?.preventDefault()
+    console.log(content)
+  }
+
+  const handleSubmitClick = () => {
+    handleSubmit()
+  }
+
   return (
-    <form className={styles.search}>
+    <form className={styles.search} onSubmit={handleSubmit}>
       <div className={styles.iconBox}>
         <BiSearchAlt2 size='24px' />
       </div>
-      <input type='text' className={styles.input} />
-      <button className={styles.searchBtn} type='button'>
+      <input type='text' className={styles.input} value={content} onChange={handleContentChange} />
+      <button className={styles.searchBtn} type='button' onClick={handleSubmitClick}>
         검색
       </button>
     </form>
---
+++ src/services/naverSearch.ts
@@ -0,0 +1,15 @@
+// import axios from 'axios'
+
+// export const getSearchData = () => {
+//     return axios.get(process.env.REACT_APP_NAVER_BASE_URL, {
+//       headers: {
+//         'X-Naver-Client-Id': process.env.REACT_APP_NAVER_ID_KEY,
+//         'X-Naver-Client-Secret': process.env.REACT_APP_NAVER_SECRET_KEY
+//       },
+//       params: {
+//         query: "",
+//         display: 20
+//         // sort => 받아오는 데이터를 어떻게 정렬할지 ex) 가격순 인기순 등등 ..
+//       }
+//     }
+// }
